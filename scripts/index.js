ebd43224017d6e016b4bd5b9c05b25cc7fdf7a4a
allex8402 <120774992+allex8402@users.noreply.github.com>
2023-03-10 18:37:39 +0800

    править замечания снова

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 let popup = document.querySelector('.popup');
 let openButton = document.querySelector('.profile__rectangle');
 let closeButton = popup.querySelector('.popup__close-icon');
-let nameInput = popup.querySelectorAll('.popup__input_info_value');
-let jobInput = popup.querySelectorAll('.popup__input_info_value');
+let nameInput = popup.querySelector('.popup__input_type_name');
+let jobInput = popup.querySelector('.popup__input_type_job');
 let nameTitle = document.querySelector('.profile__title');
 let jobTitle = document.querySelector('.profile__subtitle');
 let formElement = document.querySelector('.popup__form');
@@ -10,8 +10,8 @@ let formElement = document.querySelector('.popup__form');
 
 function openPopup() {
   popup.classList.add('popup_opened');
-  nameInput[0].value = nameTitle.textContent; //заполнить поля формы значениями на странице
-  jobInput[1].value = jobTitle.textContent;
+  nameInput.value = nameTitle.textContent; //заполнить поля формы значениями на странице
+  jobInput.value = jobTitle.textContent;
 }
 openButton.addEventListener('click', openPopup);
 
@@ -24,8 +24,8 @@ closeButton.addEventListener('click', closePopup);   /*открытие закр
 
 function handleFormSubmit(evt) {                 //обработать отправку формы
   evt.preventDefault();
-  nameTitle.textContent = nameInput[0].value;
-  jobTitle.textContent = jobInput[1].value;
+  nameTitle.textContent = nameInput.value;
+  jobTitle.textContent = jobInput.value;
   closePopup()
 }
 formElement.addEventListener('submit', handleFormSubmit);       //следить за событием отправки
