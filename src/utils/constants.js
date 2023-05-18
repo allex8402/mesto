6c7d8d98d5293a3c34a00a83340b792d5f458804
allex8402 <120774992+allex8402@users.noreply.github.com>
2023-05-18 12:07:39 +0800

    error

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,28 +12,28 @@ export const {
   buttonOpenAddCardPopup: document.querySelector('.profile__button'),
   initialCards: [
     {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+      name: 'Градиент',
+      link: 'https://gratisography.com/wp-content/uploads/2023/04/gratisography-neon-lightbulb-free-stock-photo-800x525.jpg',
     },
     {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+      name: 'Типа лето',
+      link: 'https://gratisography.com/wp-content/uploads/2023/03/gratisography-cat-thief-stock-photo-800x525.jpg',
     },
     {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+      name: 'Планы',
+      link: 'https://gratisography.com/wp-content/uploads/2023/01/gratisography-frog-racer-free-stock-photo-800x525.jpg',
     },
     {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+      name: 'Жизнь',
+      link: 'https://images.pexels.com/photos/16499857/pexels-photo-16499857.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load',
     },
     {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+      name: 'Счастье',
+      link: 'https://cdn.pixabay.com/photo/2017/09/17/08/49/elephants-2757831_640.png',
     },
     {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+      name: 'Свежесть',
+      link: 'https://plus.unsplash.com/premium_photo-1683288081743-5161d099f16e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzOHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60',
     },
   ],
 };
